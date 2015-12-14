69e266ebe70b15b28110d948510eabb662c85a1e
theghostbel <nemiga@gmail.com>
2015-12-14 12:03:50 +0300

    Add dictionary

--- index.js
+++ index.js
@@ -13,6 +13,39 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/api/dictionary', function(req, res){
+  res.json({ 
+  	nouns: [
+		'time',
+		'person',
+		'year',
+		'way',
+		'day',
+		'thing',
+		'man',
+		'world',
+		'life',
+		'hand'
+  	],
+  	adjectives: [
+		'different',
+		'used',
+		'important',
+		'every',
+		'large',
+		'available',
+		'popular',
+		'able',
+		'basic',
+		'known',
+		'various',
+		'difficult',
+		'several',
+		'united'
+  	]
+  })
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
