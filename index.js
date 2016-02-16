bc71598205705a708870e2c9272553ec3394d5e4
Victor Rusakovich <nemiga@gmail.com>
2016-02-16 21:14:08 +0300

    Ask for json() form body-parser

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
